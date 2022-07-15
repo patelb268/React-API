dd41d626aa4c7180073e27e8968cc9ed088fbc32
Prayag <patel.prayag.b@gmail.com>
2022-07-15 08:53:29 -0700

    Updates

---
+++ precache-manifest.661913dc2ff312959cdd801158a55b5a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee807e51f8f336cc0c2774aa9897962",
+    "url": "/React-API/index.html"
+  },
+  {
+    "revision": "1ee48fa47c9f05ef9b83",
+    "url": "/React-API/static/css/main.5bc4748a.chunk.css"
+  },
+  {
+    "revision": "4d3f682d6764570c9d73",
+    "url": "/React-API/static/js/2.81d50f44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-API/static/js/2.81d50f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee48fa47c9f05ef9b83",
+    "url": "/React-API/static/js/main.b1f23530.chunk.js"
+  },
+  {
+    "revision": "0ef1281d207866071423",
+    "url": "/React-API/static/js/runtime-main.1c267990.js"
+  },
+  {
+    "revision": "ac2f550a2d13dc43f77927c6e638214a",
+    "url": "/React-API/static/media/6.ac2f550a.jpg"
+  },
+  {
+    "revision": "af03d4cafdf90f21ce2a58f341c537a5",
+    "url": "/React-API/static/media/7.af03d4ca.jpg"
+  },
+  {
+    "revision": "4d1d742cc664a5d530734ac5926990ff",
+    "url": "/React-API/static/media/burger-background2.4d1d742c.jpg"
+  },
+  {
+    "revision": "b4dff14d81389b0ec7a4f2fc3719ffa9",
+    "url": "/React-API/static/media/burger-background4.b4dff14d.png"
+  },
+  {
+    "revision": "9686fd5a6e9f3673611a8888da474dc3",
+    "url": "/React-API/static/media/heading.9686fd5a.png"
+  }
+]);
